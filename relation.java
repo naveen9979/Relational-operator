6900680b2b4c9862d2ac33335338ddde9a85eb1a
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-06 22:52:14 -0700

    Add files via upload

---
+++ relation.java
@@ -0,0 +1,29 @@
+public class relation{
+    public static void main(String[] args) {
+        int W = 23 ;
+        int V = 23;
+        System.out.println(W == V);
+
+        int a = 500;
+        int b = 500;
+        int c = 678;
+        int d = 786;
+        System.out.println(a != b);
+        System.out.println(c != d);
+
+
+        int m = 500;
+        int N= 500;
+        int o = 678;
+        System.out.println(m < o);
+        System.out.println(o > N);
+
+
+        int n = 98;
+        int s = 56;
+        int l = 687;
+        System.out.println(s >= n);
+        System.out.println(l <= s);
+     }
+    
+}
